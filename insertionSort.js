b8bd77388b75a37b557df0ef4e7dc1dacfa40bae
Saket Vatsa <vatsa.saket@gmail.com>
2020-02-07 13:13:25 +0530

    Shell Sort

--- insertionSort.js
+++ insertionSort.js
@@ -1,4 +1,5 @@
-let arrNum = [29, 20, 73, 34, 64, 14,16,10,8,7,9,3,2,4,1];
+let arrNum = [35,33,42,19,27,44,29,20,73,34,64,14,16,10,8,7,9,3,2,4,1,62,83,18,53,17,95,86,47,69,25,28];
+let swapCounter =0; 
 function insertionSort(arr){
 	const arrLength = arr.length;
 	for (let i = 1; i < arrLength; i++) {
@@ -13,6 +14,7 @@ function insertionSort(arr){
 				continue;
 			}else{
 				for (let j = 1; j < i; j++) {
+					swapCounter+=1;
 					if(arr[i]>arr[j-1] && arr[i]<arr[j]){
 						let temp = arr[j]
 						arr[j] = arr[i];
@@ -27,3 +29,4 @@ function insertionSort(arr){
 }
 insertionSort(arrNum);
 console.log(arrNum)
+console.log(swapCounter)
