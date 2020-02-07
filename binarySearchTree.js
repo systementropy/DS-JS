4bebde4c1b72e2e9bf546b6131ba18a322c81e1f
Saket Vatsa <vatsa.saket@gmail.com>
2020-02-07 19:05:41 +0530

    BST base file

---
+++ binarySearchTree.js
@@ -0,0 +1,14 @@
+class Node{
+	constructor(data){
+		this.data = data;
+		this.left = null;
+		this.right = null;
+		this.parent = null;
+	}
+}
+
+class BinarySearchTree{
+	constructor(){
+		this.root = null;
+	}
+}
